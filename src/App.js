a98cec1fa7add2c81a06db1ab7968aa771d827fa
victormmorales <victorfasrev@gmail.com>
2022-01-04 12:06:21 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// Componentes:
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
+
 function App() {
   return (
-    <h1>Merntask</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
     );
 }
 
