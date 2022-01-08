d52dc4728d3cec1a31a00204012f98f7f4f8b632
victormmorales <victorfasrev@gmail.com>
2022-01-08 19:30:39 +0100

    reducer

---
+++ src/context/proyectos/proyectoState.js
@@ -0,0 +1,14 @@
+import React, { useReducer } from 'react';
+
+import proyectoContext from './proyectoContext';
+import proyectoReducer from './proyectoReducer';
+
+const proyectoState = props => {
+    const initialState = {
+        nuevoProyecto : false
+    }
+
+    // Dispatch para ejecutar las acciones
+    const [state, dispatch] = useReducer(proyectoReducer, initialState)
+
+}
